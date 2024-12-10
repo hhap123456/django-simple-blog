7c92dc7927c203c6688e1896e6c367d595e6617a
frdd <hossein.haj85@gmail.com>
2024-12-10 12:34:23 +0330

    فصل دوم-تا ایجاد template های لیست و جزئیات

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Post
+
 
 # Create your views here.
+def index(request):
+    pass
+
+def post_list(request):
+    posts = Post.published.all()
+
+    contex = {'posts': posts}
+    return render(request, "blog/list.html", contex)
+
+def post_detail(request, pk):
+    # try:
+    #     post = Post.published.get(id=pk)
+    # except:
+    #     raise Http404("No post found!")
+
+    post = get_object_or_404(Post, id=pk, status=Post.Status.PUBLISHED)
+    contex = {'post': post}
+
+    return render(request, "blog/detail.html", contex)
