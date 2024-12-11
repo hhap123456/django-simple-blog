81f2f7789076a6614c210a861bc1636c4b2fdd05
frdd <hossein.haj85@gmail.com>
2024-12-11 19:50:39 +0330

    فصل چهارم: صفحه بندی (pagination)

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
-
+from django.core import paginator
 
 # Create your views here.
 def index(request):
@@ -9,8 +10,19 @@ def index(request):
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)  # Pagination
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
 
-    contex = {'posts': posts}
+    contex = {
+        'posts': posts
+    }
+
     return render(request, "blog/list.html", contex)
 
 def post_detail(request, pk):
