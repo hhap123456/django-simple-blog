dc0310f4ce285af3785c413f611d29502c953d62
frdd <hossein.haj85@gmail.com>
2024-12-14 16:20:35 +0330

    فصل چهارم: ایجاد تمپلیت تگ سفارشی (simple tag))

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from django_jalali.templatetags.jformat import jformat
+
+from ..models import Post, Comment
+
+register = template.Library()
+
+@register.simple_tag()
+def total_posts():
+    return Post.published.count()
+
+@register.simple_tag()
+def total_comments():
+    return Comment.objects.filter(active=True).count()
+
+@register.simple_tag()
+def last_post():
+    return jformat(Post.published.last().publish, "%Y/%M/%d - %H:%I")
