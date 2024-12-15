d6587144faf6d4e08f686a9017a8fb3c00a5bd52
frdd <hossein.haj85@gmail.com>
2024-12-15 22:03:55 +0330

    فصل چهارم: ایجاد تمپلیت فیلتر سفارشی و کار با markdown

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,6 +2,8 @@ from django import template
 from django_jalali.templatetags.jformat import jformat
 from django.db.models import Count
 from ..models import Post, Comment
+from markdown import markdown
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -29,3 +31,7 @@ def latest_post(count=5):
 @register.simple_tag()
 def most_popular_posts(count=5):
     return Post.published.annotate(comments_count=Count('comments')).order_by('-comments_count')[:count]
+
+@register.filter(name='markdown')
+def to_markdown(value):
+    return mark_safe(markdown(value))
