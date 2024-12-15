dff1d9863b7bb094ed62d060e694abb063c7e596
frdd <hossein.haj85@gmail.com>
2024-12-15 22:23:40 +0330

    فصل چهارم: استایل دهی فرم های آماده

--- blog/forms.py
+++ blog/forms.py
@@ -9,7 +9,10 @@ class TicketForm(forms.Form):
 
     )
     massage = forms.CharField(widget=forms.Textarea, required=True)
-    name = forms.CharField(max_length=250, required=True)
+    name = forms.CharField(max_length=250, required=True, widget=forms.TextInput(attrs={'placeholder':"نام",
+                                                                                        'style':'height:50%;width:100a%;',
+                                                                                        'class':'name',
+                                                                                        }))
     email = forms.EmailField()
     phone = forms.CharField(max_length=11, required=True)
     subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
@@ -30,6 +33,14 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'body')
+        widgets = {
+            'body':forms.TextInput(attrs={'placeholder':'متن',
+                                          'class':'cm-body',
+                                          }),
+            'name': forms.TextInput(attrs={'placeholder':'نام',
+                                           'class': 'cm-body',
+                                           }),
+        }
 
     def clean_name(self):
         name = self.cleaned_data['name']
