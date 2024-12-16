cd5cd36c5f0b4afd58f0d67bf1a53097f820abc0
frdd <hossein.haj85@gmail.com>
2024-12-16 23:29:37 +0330

    فصل پنجم: اتصال دیتابیس به پروژه و انتقال داده های دیتابیس قبلی

--- sabzweb/settings.py
+++ sabzweb/settings.py
@@ -78,10 +78,20 @@ WSGI_APPLICATION = 'sabzweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog_admin',
+        'PASSWORD': 'fardad2643361@!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
